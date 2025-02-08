c1cb6520f67c05dd4dc1ae79087f3676b3393a20
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-02-08 11:44:07 +0100

    add upload and download file controller functions

---
+++ controllers/uploadController.js
@@ -0,0 +1,48 @@
+import FileUpload from "../models/fileUploadSchema.js";
+
+const uploadFile = async (req, res) => {
+  try {
+    const file = new FileUpload({
+      filename: req.file.originalname,
+      contentType: req.file.mimetype,
+      size: req.file.size,
+      data: req.file.buffer,
+    });
+
+    const savedFile = await file.save();
+    res.status(201).json({
+      success: true,
+      fileId: savedFile._id,
+      data: savedFile,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const downloadFile = async (req, res) => {
+  try {
+    const file = await FileUpload.findById(req.params.id);
+    if (!file) {
+      return res.status(404).json({
+        success: false,
+        message: "File not found",
+      });
+    }
+    res.set({
+      "Content-Type": file.contentType,
+      "Content-Length": file.size,
+    });
+    res.send(file.data);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { uploadFile, downloadFile };
